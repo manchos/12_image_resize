bce6c380ded094c530be0682800fcb9c633eacbf
manchos <vladm.ru@gmail.com>
2018-01-03 15:40:25 +0300

    removed the unnecessary

--- image_resize.py
+++ image_resize.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.CRITICAL)
 
 
-def set_default_file_name(image, new_image):
+def get_default_file_name(image, new_image):
     width, height = new_image.size
     file_extension = image.format.lower()
     return '{}__{}x{}.{}'.format(
@@ -28,8 +28,8 @@ def scale_image_size(image, new_scale):
 
 
 def apply_aspect_ratio_to_height_dialog():
-    enter_height = \
-        input('The height does not match to the aspect ratio. To enter this value? (yes, no)')
+    enter_height = input('The height does not match to the aspect ratio. '
+                         'To enter this value? (yes, no)')
     if enter_height in ('', 'yes'):
         return True
     elif enter_height == 'no':
@@ -39,32 +39,23 @@ def apply_aspect_ratio_to_height_dialog():
 def get_valid_height(image, new_width, new_height=0,
                      apply_aspect_ratio=True):
     aspect_ratio = image.size[1] / image.size[0]
-    valid_height = int(round(aspect_ratio * new_width))
+    aspect_ratio_height = int(round(aspect_ratio * new_width))
     if not new_height:
-        return valid_height
-    # height not matches aspect ratio:
-    if new_height != valid_height:
+        return aspect_ratio_height
+    if new_height != aspect_ratio_height:
         if not apply_aspect_ratio:
             return new_height
-        return valid_height
+        return aspect_ratio_height
 
 
-def check_source_img_path(img_path, img_formats):
-    if os.path.isfile(img_path) and check_file_extension(img_path, img_formats):
-        return True
+def get_valid_size(size):
+    if int(size) > 0:
+        return int(size)
     else:
-        return False
+        raise argparse.ArgumentTypeError('The size may be integer number and > 0!')
 
 
-def valid_size(width, height):
-    if all([type(size) is int and size >= 0 for size in (width, height)]):
-        return True
-    else:
-        logging.error('Width and height must be integer type and >= 0')
-        return False
-
-
-def valid_scale(scale):
+def get_valid_scale(scale):
     if float(scale) > 0:
         return float(scale)
     else:
@@ -85,10 +76,6 @@ def get_valid_image_size(image, width, height, default_width):
 
 
 def resize_image(image, scale, width, height, default_width):
-    if not valid_size(width, height) or (scale > 1 and (height or width)):
-        if scale > 1:
-            logging.error('The scale x{} was defined!.\n Resize is not possible!'.format(scale))
-        return None
     if scale == 1:
         width, height = get_valid_image_size(image, width, height, default_width)
     else:
@@ -99,11 +86,11 @@ def resize_image(image, scale, width, height, default_width):
 def set_valid_ouput_path(image, output_path, img_formats):
     if output_path:
         if os.path.isdir(output_path):
-            new_image_name = set_default_file_name(image, new_image)
+            new_image_name = get_default_file_name(image, new_image)
             return os.path.join(output_path, new_image_name)
         else:
-            if os.path.split(output_path)[0] == os.path.split(image.filename)[0] or \
-                    os.path.isdir(os.path.split(output_path)[0]):
+            if (os.path.split(output_path)[0] == os.path.split(image.filename)[0] or
+                    os.path.isdir(os.path.split(output_path)[0])):
                 if check_file_extension(os.path.split(output_path)[1], img_formats):
                     return output_path
                 else:
@@ -111,29 +98,18 @@ def set_valid_ouput_path(image, output_path, img_formats):
                     return None
     else:
         return os.path.join(os.path.dirname(image.filename),
-                            set_default_file_name(image, new_image))
-
-
-def sizes_validating(scale, width, height):
-    is_valid, valid_message = True, ''
-    if not valid_size(width, height):
-        is_valid = False
-        valid_message = 'Sizes must be positive numbers.'
-    if scale > 1 and (height or width):
-        is_valid = False
-        valid_message += 'The scale x{} was defined!.\n Resize is not possible! '.format(scale)
-    return is_valid, valid_message
+                            get_default_file_name(image, new_image))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Displays information about 20 random curses from coursera.org")
     parser.add_argument('img_path', help='set path to image file to resize')
-    parser.add_argument('-width', '--width', default=0, type=int, dest="width",
+    parser.add_argument('-width', '--width', default=0, type=get_valid_size, dest="width",
                         help='set image width (integer>0)')
-    parser.add_argument('-height', '--height', default=0, type=int, dest="height",
+    parser.add_argument('-height', '--height', default=0, type=get_valid_size, dest="height",
                         help='set image height (integer>0)')
-    parser.add_argument('-scale', '--scale', default=1, type=valid_scale, dest="scale",
+    parser.add_argument('-scale', '--scale', default=1, type=get_valid_scale, dest="scale",
                         help='set image scale (fractional number > 0)')
     parser.add_argument(
         '-output', '--output', default='', dest="output_path", help='set image path to save')
@@ -141,28 +117,36 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    cli_args = parse_args()
+    args = parse_args()
     img_formats = ('.jpg', '.jpeg', '.png')
 
-    if not check_source_img_path(cli_args.img_path, img_formats=img_formats):
-        print('The specified file extension is not supported. '
-              'Use files with the file extension:{}'.
+    if (not os.path.isfile(args.img_path) or
+            not check_file_extension(args.img_path, img_formats_tuple=img_formats)):
+        print('Check the file path and file extension. Use files with the file extension:{}'.
               format(', '.join(img_formats)))
     else:
-        image = Image.open(cli_args.img_path)
-        scale, width, height = cli_args.scale, cli_args.width, cli_args.height
-        validating, message = sizes_validating(scale, width, height)
-        if not validating:
-            print(message)
+        image = Image.open(args.img_path)
+        scale, width, height = args.scale, args.width, args.height
+        if scale > 1 and (height or width):
+            print('The scale x{} was defined!.\n Resize with the width and height is not possible!'
+                  .format(scale))
         else:
             valid_height = get_valid_height(
-                image, width, height, apply_aspect_ratio=apply_aspect_ratio_to_height_dialog())
-            new_image = resize_image(image, scale, width, valid_height, default_width=200)
+                image, width, height,
+                apply_aspect_ratio=apply_aspect_ratio_to_height_dialog(),
+            )
+            new_image = resize_image(
+                image, scale, width,
+                height=valid_height,
+                default_width=200
+            )
             try:
                 output_path = set_valid_ouput_path(
-                    image, cli_args.output_path, img_formats=img_formats)
+                    image, args.output_path,
+                    img_formats=img_formats
+                )
                 new_image.save(output_path)
             except (IOError, ValueError, KeyError) as exc:
-                print('{} File {} could not be written.'.format(exc, cli_args.output_path))
+                print('{} File {} could not be written.'.format(exc, args.output_path))
             else:
                 print('file save in {} '.format(output_path))
