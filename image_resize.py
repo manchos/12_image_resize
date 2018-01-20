5d5cc440e1b8bbea141d692c70f5d48552d9c004
manchos <vladm.ru@gmail.com>
2018-01-20 23:35:43 +0300

    formatting fix combine functions for get_new_image_size

--- image_resize.py
+++ image_resize.py
@@ -7,20 +7,20 @@ import logging
 logging.basicConfig(level=logging.CRITICAL)
 
 
-def get_default_image_name(width, height, filename):
+def get_new_image_name(width, height, filename):
     filename_without_ext, file_extension = os.path.splitext(filename)
-    return '{}__{}x{}{}'.format(filename_without_ext, width, height, file_extension)
+    return '{}__{}x{}{}'.format(filename_without_ext, width,
+                                height, file_extension)
 
 
 def check_file_extension(file_path, img_formats_set):
-    filename = os.path.basename(file_path)
-    file_extension = os.path.splitext(filename)[1]
+    file_extension = os.path.splitext(file_path)[1]
     return file_extension in img_formats_set
 
 
 def apply_aspect_ratio_to_height_dialog():
-    enter_height = input('The height does not match to the aspect ratio.\
-     To enter this value? (yes, no)')
+    enter_height = input('The height does not match to the aspect ratio.'
+                         'To enter this value? (yes, no)')
     if enter_height in ('', 'yes'):
         return False
     elif enter_height == 'no':
@@ -31,72 +31,78 @@ def validate_size(size):
     if int(size) > 0:
         return int(size)
     else:
-        raise argparse.ArgumentTypeError('The size may be integer number and > 0!')
+        raise argparse.ArgumentTypeError(
+            'The size may be integer number and > 0!')
 
 
 def validate_scale(scale):
     if float(scale) > 0:
         return float(scale)
     else:
-        raise argparse.ArgumentTypeError('The scale may be fractional number and > 0!')
+        raise argparse.ArgumentTypeError(
+            'The scale may be fractional number and > 0!')
 
 
 def check_output_path(output_path, img_formats_set):
     dir, file_name = os.path.split(output_path)
     if dir and not os.path.isdir(dir) or (
-                file_name and not check_file_extension(file_name, img_formats_set)):
+                file_name and
+                not check_file_extension(file_name, img_formats_set)):
         return False
     return True
 
 
-def get_image_size(image, width, height, scale, apply_aspect_ratio=True):
+def get_new_image_size(image, width, height, scale, apply_aspect_ratio=True):
     if scale is not None:
-        return image.size, scale
+        return image.size[0], image.size[1], scale
     scale = 1
-    width, height = get_width_height(image, width, height, apply_aspect_ratio)
-    return width, height, scale
-
-
-def get_width_height(image, width, height, apply_aspect_ratio):
-    aspect_ratio_height = int(round(image.size[1] / image.size[0] * width)) \
-        if width is not None else None
-    if any(width, height) and height != aspect_ratio_height:
+    if width is None:
+        width = int(round(image.size[0] / image.size[1] * height))
+    elif height is None:
+        height = int(round(image.size[1] / image.size[0] * width))
+    elif height != int(round(image.size[1] / image.size[0] * width)):
         if callable(apply_aspect_ratio):
             apply_aspect_ratio = apply_aspect_ratio()
         if apply_aspect_ratio:
-            height = aspect_ratio_height
-    elif height is None:
-        height = aspect_ratio_height
-    elif width is None:
-        width = int(round(image.size[0] / image.size[1] * height))
-    return width, height
+            height = int(round(image.size[1] / image.size[0] * width))
+    return width, height, scale
 
 
 def check_valid_args(args):
     if (not os.path.isfile(args.img_path) or
-            not check_file_extension(args.img_path, img_formats_set=img_formats) or
-            not check_output_path(args.output_path, img_formats_set=img_formats)):
-        print('Check the file path and file extension. Use files with the file extension:{}'.
+        not check_file_extension(args.img_path, img_formats_set=img_formats) or
+            not check_output_path(
+                args.output_path,
+                img_formats_set=img_formats
+            )):
+        print('Check the file path and file extension. '
+              'Use files with the file extension:{}'.
               format(', '.join(img_formats)))
     elif not any((args.scale, args.width, args.height)):
-        print('For image resize you must enter values (-scale or -width or -height)')
+        print('For image resize you must enter values '
+              '(-scale or -width or -height)')
     elif args.scale is not None and (args.height or args.width):
         print('The scale x{} was defined!.\
-              \n Resize with the width and height is not possible!'.format(args.scale))
+              \n Resize with the width and height is not possible!'.
+              format(args.scale))
     else:
         return True
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='save image with new size in output directory default file name obtain '
-                    'from IMAGE_PATH like this: FILE_NAME__WIDTHxHEIGHT.FILE_EXTENSION')
+        description=('save image with new size in output directory default '
+                     'file name obtain from IMAGE_PATH like this: '
+                     'FILE_NAME__WIDTHxHEIGHT.FILE_EXTENSION'))
     parser.add_argument('img_path', help='set path to image file to resize')
-    parser.add_argument('-width', '--width', default=None, type=validate_size, dest="width",
+    parser.add_argument('-width', '--width', default=None,
+                        type=validate_size, dest="width",
                         help='set image width (integer>0)')
-    parser.add_argument('-height', '--height', default=None, type=validate_size, dest="height",
+    parser.add_argument('-height', '--height', default=None,
+                        type=validate_size, dest="height",
                         help='set image height (integer>0)')
-    parser.add_argument('-scale', '--scale', default=None, type=validate_scale, dest="scale",
+    parser.add_argument('-scale', '--scale', default=None,
+                        type=validate_scale, dest="scale",
                         help='set image scale (fractional number > 0)')
     parser.add_argument('-output', '--output', default='', dest="output_path",
                         help='set image path to save')
@@ -110,20 +116,32 @@ if __name__ == '__main__':
     if check_valid_args(args):
         image = Image.open(args.img_path)
         apply_aspect_ratio = apply_aspect_ratio_to_height_dialog
-        (width, height), scale = get_image_size(image, args.width, args.height, args.scale,
-                                                apply_aspect_ratio)
+        width, height, scale = get_new_image_size(
+            image,
+            args.width,
+            args.height,
+            args.scale,
+            apply_aspect_ratio,
+        )
 
         if os.path.isfile(args.output_path):
             output_path = args.output_path
         else:
-            new_image_name = get_default_image_name(width, height, os.path.basename(image.filename))
-            output_dir = os.path.dirname(image.filename) if not args.output_path else args.output_path
+            new_image_name = get_new_image_name(
+                width,
+                height,
+                os.path.basename(image.filename)
+            )
+            output_dir = (os.path.dirname(image.filename)
+                          if not args.output_path else args.output_path)
             output_path = os.path.join(output_dir, new_image_name)
 
         try:
-            new_image = image.resize((int(round(width * scale)), int(round(height * scale))))
+            new_image = image.resize((int(round(width * scale)),
+                                      int(round(height * scale))))
             new_image.save(output_path)
         except (IOError, ValueError, KeyError) as exc:
-            print('{} File {} could not be written.'.format(exc, args.output_path))
+            print('{} File {} could not be written.'.
+                  format(exc, args.output_path))
         else:
             print('file save in {} '.format(output_path))
