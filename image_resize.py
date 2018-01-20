8ccaef7131af8a19952c11a80e8160a31e0128a5
manchos <vladm.ru@gmail.com>
2018-01-21 01:29:11 +0300

    fix get_new_image_size because it is too difficult for Wallie

--- image_resize.py
+++ image_resize.py
@@ -52,6 +52,13 @@ def check_output_path(output_path, img_formats_set):
     return True
 
 
+def get_aspect_ratio_height(image, new_width, apply_aspect_ratio=True):
+    if callable(apply_aspect_ratio):
+        apply_aspect_ratio = apply_aspect_ratio()
+    if apply_aspect_ratio:
+        return int(round(image.size[1] / image.size[0] * new_width))
+
+
 def get_new_image_size(image, width, height, scale, apply_aspect_ratio=True):
     if scale is not None:
         return image.size[0], image.size[1], scale
@@ -59,12 +66,9 @@ def get_new_image_size(image, width, height, scale, apply_aspect_ratio=True):
     if width is None:
         width = int(round(image.size[0] / image.size[1] * height))
     elif height is None:
-        height = int(round(image.size[1] / image.size[0] * width))
+        height = get_aspect_ratio_height(image, width)
     elif height != int(round(image.size[1] / image.size[0] * width)):
-        if callable(apply_aspect_ratio):
-            apply_aspect_ratio = apply_aspect_ratio()
-        if apply_aspect_ratio:
-            height = int(round(image.size[1] / image.size[0] * width))
+        height = get_aspect_ratio_height(image, width, apply_aspect_ratio)
     return width, height, scale
 
 
